5bed4daa1f454ecdcb81df67801f1564f7a87120
Michael Kim <m.kim4247@gmail.com>
2018-09-12 21:22:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -115,9 +115,9 @@ function createRock(x) {
  */
 function endGame() {
   clearInterval(gameInterval);
-  for (let i = 0; i < ROCKS.length; i++) {
-    ROCKS.removeChild(i)
-  }
+ 
+  
+      
   GAME.removeEventListener('keydown', moveDodger);
   alert("YOU LOSE!");
 }
