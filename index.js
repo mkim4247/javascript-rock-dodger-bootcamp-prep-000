92183985c20bd941a6e07dcddde8213294ede691
Michael Kim <m.kim4247@gmail.com>
2018-09-12 21:34:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -156,6 +156,13 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+  var right = positionToInteger(DODGER.style.right)
+  if (right < 400) {
+  DODGER.style.right = `${right += 4}px`
+  window.requestAnimationFrame(moveDodgerRight)
+  }
+   
+   
 }
 
 /**
